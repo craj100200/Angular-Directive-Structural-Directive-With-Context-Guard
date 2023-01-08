df6fbae2f4ce0fde6bbdbe5dfbb36c8f705f3a52
rajesh <kemar.dev1@outlook.com>
2023-01-08 23:40:27 +0530

    First Push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TrigonometryDirective } from './trigonometry.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrigonometryDirective
   ],
   imports: [
     BrowserModule
